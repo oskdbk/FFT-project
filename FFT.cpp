2547c35b92764ec9d18e869ac99bd34f61f2ea48
Oskar Dabkowski <oskar.dabkowski@polytechnique.edu>
2024-05-17 17:47:06 +0200

    First file

---
+++ FFT.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <climits>
+#include <thread>
+#include <numeric>
+#include <iterator>
+#include <optional>
+#include <vector>
+
+typedef long double Num;
+typedef std::vector<long double>::const_iterator NumIter;
+
+//-----------------------------------------------------------------------------
+
+

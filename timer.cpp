8b435f006acf08d7ec050e93a34d4c5243e62a84
Tita Rosemeyer <91255842+Tita-Rosemeyer@users.noreply.github.com>
2024-06-08 23:01:51 +0200

    use arguments for timer.cpp
    can specify length of input, max number of threads and number of iterations for avg timing

--- timer.cpp
+++ timer.cpp
@@ -7,18 +7,34 @@ using namespace std;
 
 
 #ifndef NO_MAIN
-int main(){
+int main(int argc, char* argv[]){
     // This just generates a vector of length 15 of (1, 0)
     // vector<complex_num> P(15, complex_num(1, 0));
-
-    vector<complex_num> P = Read_CSV("Weather_data.csv");
+    vector<complex_num> P;
+    int iters = 5;
+    int max_threads = 24;
+    if (argc == 1){
+        P = Read_CSV("Weather_data.csv");
+    } else {
+        int n = stoi(argv[1]);
+        P =  std::vector<complex_num>(40320, complex_num(1.0, 0.0));
+    }
+    if (argc > 2){
+        max_threads = stoi(argv[2]);
+    }
+    if (argc > 3){
+        iters = stoi(argv[3]);
+    }
     int n = P.size();
+    
+    cout << "Running with n = " << n << ", max_threads = " << max_threads << " and iters = " << iters << endl;
+    
+
     // copy P to Q
     vector<complex_num> Q(P.size());
     for(int i = 0; i < P.size(); i++){
         Q[i] = P[i];
     }
-    // cout<<P.size()<<endl;
     
 
     // measure time of both versions
@@ -26,49 +42,50 @@ int main(){
     // sequential version
     clock_t start, end;
     double avg_time = 0;
-    for (int i = 0; i < 30; i++)
+    for (int i = 0; i < iters; i++)
     {
         start = clock();
         vector<complex_num> P_star = GeneralFFT(P, false);
         end = clock();
         avg_time += double(end - start);
     }
-    avg_time /= 30;
+    avg_time /= iters;
 
     cout << "Sequential version took " <<  avg_time << " ticks" << endl;
 
     // inplace version
     avg_time = 0;
-    for (int i = 0; i < 30; i++)
+    for (int i = 0; i < iters; i++)
     {
         start = clock();
         vector<complex_num> P_star_inplace = GeneralFFT_inplace(Q, false);
         end = clock();
         avg_time += double(end - start);
     }
-    avg_time /= 30;
+    avg_time /= iters;
 
     cout << "Inplace version took " << avg_time << " ticks" << endl;
     
     // try with different number of threads
-    for(int i = 1; i <= 30; i++){
+    for(int i = 1; i <= max_threads; i++){
         if (n % i != 0){
             continue;
         }
         avg_time = 0;
-        for(int j = 0; j < 30; j++){
+        for(int j = 0; j < iters; j++){
             start = clock();
             vector<complex_num> P_star_parallel = GeneralFFT_Parallel(Q, i);
             end = clock();
             avg_time += double(end - start);
         }
-        avg_time /= 30;
+        avg_time /= iters;
         cout << "Parallel version with " << i << " threads took " << avg_time << " ticks" << endl;
     }
 
 
     vector<complex_num> P_star = GeneralFFT(P, false);
-    vector<complex_num> P_star_parallel = GeneralFFT_Parallel(Q, 20);
+    vector<complex_num> P_star_parallel = GeneralFFT_Parallel(Q, max_threads);
+    vector<complex_num> P_star_inplace = GeneralFFT_inplace(Q, false);
     size_t t = P_star.size();
     for(int i = 0; i < t; i++){
         if(norm(P_star[i] - P_star_parallel[i]) > 1e-6){

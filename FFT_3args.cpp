1021147a78f29ec673930a60260481413d8a2d33
Natali124 <nataligogishvili@gmail.com>
2024-06-10 02:03:45 +0200

    slightly improved

--- FFT_3args.cpp
+++ FFT_3args.cpp
@@ -18,8 +18,6 @@
 using namespace std;
 typedef complex<double> complex_num;
 
-vector<complex_num> FFT(vector<complex_num>& input);
-
 /*
 Basic DFT
 */
@@ -74,20 +72,74 @@ vector<complex_num> FFT(vector<complex_num>&x, int N1, int N2, int N3) {
         }
     }
     // Step 3
-    complex_num X2[N1][N2][N3];
+    complex_num X3[N1][N2][N3];
     for (int k1 = 0; k1<N1; k1++){
         for (int k2 = 0; k2<N2; k2++){
             for (int k3 = 0; k3<N3; k3++){
                 complex_num sm = complex_num(0,0);
                 for (int j3 = 0; j3 < N3; j3++){
-                    sm += X1[j3][k1][k2]*polar(1.0, 2.0 * M_PI * (j3*k1+k2*N1+k3*N2) / (N1*N2*N3))*polar(1.0, 2.0 * M_PI * (j3*k3) / N3);
+                    sm += X2[j3][k1][k2]*polar(1.0, 2.0 * M_PI * (j3*k1+k2*N1+k3*N2) / (N1*N2*N3))*polar(1.0, 2.0 * M_PI * (j3*k3) / N3);
                 }
-                X2[k1][k2][k3] = sm;
+                X3[k1][k2][k3] = sm;
             }
         }
     }
 
-    vector<complex_num> output;
-    
+    vector<complex_num> output(x.size());
+    // Flatten - pretty sure this is wrong
+    for (int i1 = 0; i1<N1; i1++){
+        for (int i2 = 0; i2<N2; i2++){
+            for (int i3 = 0; i3<N3; i3++){
+                output[i3 + i2*N3 + i1*N3*N2] = X3[i1][i2][i3];
+            }
+        }
+    }
     return output;
+}
+
+void printVector(const vector<complex_num>& vec) {
+    for (const auto& c : vec) {
+        cout << c << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    // Test case 1:
+    vector<complex_num> input1 = {1.0, 1.0, 1.0, 1.0};
+    cout << endl << "Input 1: " << endl;
+    vector<complex_num> output1 = DFT(input1);
+    printVector(output1);
+    cout << endl;
+    vector<complex_num> output1ct = FFT(input1, 2, 2, 1);
+    printVector(output1ct);
+
+    // Test case 2:
+    vector<complex_num> input2 = {1.0, 2.0, 3.0, 4.0};
+    cout << endl << "Input 2: " << endl;
+    vector<complex_num> output2 = DFT(input2);
+    printVector(output2);
+    cout << endl;
+    vector<complex_num> output2ct = FFT(input2, 2, 2, 1);
+    printVector(output2ct);
+
+    // Test case 3:
+    vector<complex_num> input3 = {0.0, 1.0, 0.0, -1.0};
+    cout << endl << "Input 3: " << endl;
+    vector<complex_num> output3 = DFT(input3);
+    printVector(output3);
+    cout << endl;
+    vector<complex_num> output3ct = FFT(input3, 2, 2, 1);
+    printVector(output3ct);
+
+    // Test case 4:
+    vector<complex_num> input4 = {1.0, 2.0, 3.0, 4.0};
+    cout << endl << "Input 4: " << endl;
+    vector<complex_num> output4 = DFT(input4);
+    printVector(output4);
+    cout << endl;
+    vector<complex_num> output4ct = FFT(input4, 2, 2, 1);
+    printVector(output4ct);
+
+    return 0;
 }
